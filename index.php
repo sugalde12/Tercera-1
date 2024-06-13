5024e70ceca1f8e0ad4571c725d7cd62c29bfc3a
Your Name <you@example.com>
2024-06-12 22:16:52 -0600

    Commit inicial con todos los archivos del proyecto

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php require_once "./parte_superior.php"?>
+<?php require_once "./parte_inferior.php"?>
---
+++ parte_superior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="English"> 
+
+<head>
+    <meta charset="uft-8">
+    <meta name="Proyecto de Tercera 1" content="Página de Prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+        <img src="./images/menu.png" alt="">
+</a>
+        <img src="./images/menu.png" alt="">
+        <p class="texto-menu-izquierda">
+            Mariposa
+        </p>
+
+    </div>
+</menu>
