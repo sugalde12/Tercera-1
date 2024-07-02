9445d4c8b14694d1e0e4d744ed391f5bbc30f858
Your Name <you@example.com>
2024-07-01 21:30:41 -0600

    Cards

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     </div>
     <div class="main-accordion-container">
     </div>
+    <div class="card-list">
+    </div>
+    
 </body>
 
 <?php require_once "./parte_inferior.php"?>
--- parte_inferior.php
+++ parte_inferior.php
@@ -1,2 +1,3 @@
 <script src="./script/hamburguer-icon.js"></script>
 <script src="./script/accordion.js"></script>
+<script src="./script/cards.js"></script>
