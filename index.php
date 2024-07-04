77c60b2e5f1d8601ae18b64bcf833e33e704eb32
Your Name <you@example.com>
2024-07-03 19:35:22 -0600

    Ultima clase

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
-<?php require_once "./parte_superior.php"?>
+<?php require_once "./parte_superior.php" ?>
 
 <div class="titulo">
-<p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
+    <p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
 </div>
 
 <body>
     <div class="main-img">
-    <img src="./images/clases.png" alt="">
+        <img src="./images/clases.png" alt="">
     </div>
     <div class="main-accordion-container">
     </div>
-    <div class="card-list">
+    <div class="mid-info">
+        <p class="mid-info-title">Oferta Académica</p>
+        <p class="mid-info-desc">Decídete y escoge, que quieres estudiar</p>
     </div>
-    
+    <div class="card-list">
+
+    </div>
+
 </body>
 
-<?php require_once "./parte_inferior.php"?>
+<?php require_once "./parte_inferior.php" ?>
--- parte_inferior.php
+++ parte_inferior.php
@@ -1,3 +1,20 @@
+<footer>
+    <div class="footer-izquierda">
+        <h2>Tercera 1, II Cuatrimestre, 2024</h2>
+
+    </div>
+
+    <div class="footer-derecha">
+        <p>Todos los derechos reservados</p>
+        <p>Esta es una página de prueba</p>
+        <p>2024-2025</p>
+    </div>
+</footer>
+
+
+
+
+
 <script src="./script/hamburguer-icon.js"></script>
 <script src="./script/accordion.js"></script>
 <script src="./script/cards.js"></script>
